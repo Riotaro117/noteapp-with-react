bad226af63412305513691fcf9e57974146dd07b
riotaro <rugby117117@gmail.com>
2025-12-22 05:48:21 +0900

    ノートの更新用関数の用意

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  // サイドバーのノートの状態を管理
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log('新しくノートが追加されました');
+
+    const newNote = {
+      id: 1,
+      title: '新しいノート',
+      content: '新しいノートの内容',
+      modDate: Date.now(),
+    };
+    // 元の配列をコピーして新しいオブジェクトを追加していく
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
