cbc845a1c6276db9b6aa1c1387f1d7cba74b2947
riotaro <rugby117117@gmail.com>
2025-12-24 05:34:40 +0900

    ノートを編集する機能

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import './Main.css';
+import Markdown from 'react-markdown';
 
-const Main = ({ activeNoteData }) => {
+const Main = ({ activeNoteData, onUpdateNote }) => {
+  const onEditNote = (key, value) => {
+    onUpdateNote({
+      ...activeNoteData,
+      [key]: value,
+      modDate: Date.now(),
+    });
+  };
   if (!activeNoteData) {
-    return <div className='no-active-note'>ノートが選択されていません</div>;
+    return <div className="no-active-note">ノートが選択されていません</div>;
   }
   // getActiveNote();
-  console.log(activeNoteData)
+  console.log(activeNoteData);
+
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
-        <input type="text" />
-        <textarea id="" placeholder="ノート内容を記入"></textarea>
+        <input
+          id="title"
+          type="text"
+          value={activeNoteData.title}
+          onChange={(e) => onEditNote('title', e.target.value)}
+        />
+        <textarea
+          id="content"
+          placeholder="ノート内容を記入"
+          value={activeNoteData.content}
+          onChange={(e) => onEditNote('content', e.target.value)}
+        ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNoteData.title}</h1>
-        <div className="markdown-preview">{activeNoteData.content}</div>
+        <div className="markdown-preview">
+          <Markdown>{activeNoteData.content}</Markdown>
+        </div>
       </div>
     </div>
   );
